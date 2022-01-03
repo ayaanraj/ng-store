8ff80f2934ea837fa2d089422fa12ff200910d70
ayaanraj <dhondiarunteja@gmail.com>
2022-01-03 16:21:09 +0530

    initial commit

---
+++ src/app/cart.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Product } from "./products";
+import { HttpClient } from "@angular/common/http";
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  items: Product[] = [];
+  constructor(private http: HttpClient) { }
+
+  addToCart(product:Product):void {
+    this.items.push(product);
+  }
+
+  getItems():Product[] {
+    return this.items;
+  }
+
+  clearItems() {
+    this.items = [];
+    return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
+}
